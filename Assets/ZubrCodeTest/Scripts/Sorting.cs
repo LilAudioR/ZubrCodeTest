9908c78844ec7db9f7cb496c634ed7164c6d2c5e
LilAudioR <info@lilliesimpsonlittle.xyz>
2023-08-18 22:40:53 +0100

    sorting code added and tested

--- Assets/ZubrCodeTest/Scripts/Sorting.cs
+++ Assets/ZubrCodeTest/Scripts/Sorting.cs
@@ -1,38 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-
+using Unity.VisualScripting;
+using static System.String;
 
 public class Sorting : MonoBehaviour
 {
-    public string maddison = "Maddison Albert";
-    public string hester = "Hester Blankenship";
-    public string saara = "Saara Browne";
-    public string libbie = "Libbie Case";
-    public string dominick = "Dominick Howells";
-    public string abida = "Abida Hayden";
-    public string giorgia = "Giorgia Cotton";
-    public string rita = "Rita Smyth";
-    public string rania = "Rania Wolf";
-    public string zainab = "Zainab Sheldon";
-
-
-    void LastNameDescending()
+    public List<string> names = new List<string>
     {
-        
-    }
-    void FirstNameDescending()
-    {
+        "Maddison Albert",
+        "Hester Blankenship",
+        "Saara Browne",
+        "Libbie Case",
+        "Dominick Howells",
+        "Abida Hayden",
+        "Giorgia Cotton",
+        "Rita Smyth",
+        "Rania Wolf",
+        "Zainab Sheldon"
+    };
 
-    }
-    void LastNameAscending()
+    private string GetFirstName(string fullName)
     {
-
-    }
-    void FirstNameAscending()
-    {
-
+        return fullName.Split(' ')[0];
     }
 
+    private string GetLastName(string fullName)
+    {
+        return fullName.Split(' ')[1];
+    }
+
+    public void LastNameDescending()
+    {
+        names.Sort((a, b) => Compare(GetLastName(b), GetLastName(a)));
+        PrintNames(names);
+    }
+    public void FirstNameDescending()
+    {
+        names.Sort((a, b) => Compare(GetFirstName(b), GetFirstName(a)));
+        PrintNames(names);
+    }
+    public void LastNameAscending()
+    {
+        names.Sort((a, b) => Compare(GetLastName(a), GetLastName(b)));
+        PrintNames(names);
+    }
+    public void FirstNameAscending()
+    {
+        names.Sort((a, b) => Compare(GetFirstName(a), GetFirstName(b)));
+        PrintNames(names);
+    }
+    
+    private static void PrintNames(List<string> nameList)
+    {
+        foreach (var namesToPrint in nameList)
+        {
+            Debug.Log(namesToPrint);
+        }
+    }
+    
 }
